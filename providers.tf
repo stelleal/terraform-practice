cf1542db4179925314d891aafbd447f93fd71833
Stella Leal <stellalleal@gmail.com>
2023-02-11 16:42:20 -0300

    configure s3 state

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+#   required_version = ">= 0.14.0"
+#   backend "s3" {
+#     bucket = "my-bucket"
+#     key    = "project/${var.ambiente}/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
